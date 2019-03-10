de3852ae0c0145de38608376eaa073c7d552cc2d
Vladimir <18351400+en9inerd@users.noreply.github.com>
2019-03-10 19:45:06 -0400

    some fixes for Travis passing

--- build.py
+++ build.py
@@ -14,29 +14,28 @@ from pyntcontrib import execute, safe_cd
 from semantic_version import Version
 
 PROJECT_NAME = "tgeraser"
-PIPENV = "pipenv run"
-SRC = '.'
-PYTHON = "python3.7"
+SRC = "."
+PYTHON = "python"
 IS_DJANGO = False
-IS_TRAVIS = 'TRAVIS' in os.environ  # I double we will ever use travis...
+IS_TRAVIS = "TRAVIS" in os.environ  # I double we will ever use travis...
 if IS_TRAVIS:
     PIPENV = ""
 else:
     PIPENV = "pipenv run"
-IS_APP = True  # freeze requirements
-
-MAC_LIBS = ":"
-BUILD_SERVER_LIBS = ":"
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
-from build_utils import (check_is_aws, execute_get_text,
-                         execute_with_environment, is_it_worse,
-                         skip_if_no_change, timed)
 
 CURRENT_HASH = None
 
-if check_is_aws():
-    MAC_LIBS += BUILD_SERVER_LIBS
+MAC_LIBS = ":"
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "."))
+from build_utils import (
+    check_is_aws,
+    execute_get_text,
+    execute_with_environment,
+    skip_if_no_change,
+    timed,
+)
+
 
 @task()
 @timed()
@@ -47,23 +46,41 @@ def clean_state():
             if file.startswith("last") and file.endswith(".txt"):
                 execute("rm", "-f", str(file))
 
+
 @task()
 @timed()
 def update_pip_and_pipenv():
     # env = config_pythonpath()
     execute("pip", "install", "--upgrade", "pip")
 
+
 @task()
 @timed()
 def clean():
     pass
 
+
 @task()
 @skip_if_no_change("formatting")
-@timed()
 def formatting():
     with safe_cd(SRC):
-        execute(*("{0} black {1}".format(PIPENV, PROJECT_NAME).split(" ")))
+        if sys.version_info < (3, 6):
+            print("Black doesn't work for current version of Python")
+            return
+        command = "{0} black {1}".format(PIPENV, PROJECT_NAME).strip()
+        print(command)
+        result = execute_get_text(command)
+        assert result
+        changed = []
+        for line in result.split("\n"):
+            if "reformatted " in line:
+                file = line[len("reformatted ") :].strip()
+                changed.append(file)
+        for change in changed:
+            command = "git add {0}".format(change)
+            print(command)
+            execute(*(command.split(" ")))
+
 
 @task(clean, formatting)
 @skip_if_no_change("compile_py")
@@ -72,77 +89,88 @@ def compile_py():
     with safe_cd(SRC):
         execute(PYTHON, "-m", "compileall", PROJECT_NAME)
 
-@task()
+
+@task(compile_py)
 @timed()
 def prospector():
     with safe_cd(SRC):
-        execute("pipenv",
-                *("run prospector {0} --profile tgeraser_style --pylint-config-file=pylintrc.ini --profile-path=.prospector"
-                  .format(PROJECT_NAME)
-                  .split(" ")))
+        execute(
+            "pipenv",
+            *(
+                "run prospector {0} --profile tgeraser_style --pylint-config-file=pylintrc.ini --profile-path=.prospector".format(
+                    PROJECT_NAME
+                ).split(
+                    " "
+                )
+            )
+        )
+
 
 @task(compile_py, prospector)
-@skip_if_no_change("lint", expect_files="lint.txt")
-@timed()
+@skip_if_no_change("lint")
 def lint():
+    """
+    Lint
+    """
     with safe_cd(SRC):
         if os.path.isfile("lint.txt"):
             execute("rm", "lint.txt")
-    with safe_cd(SRC):
-        # so that pylint doesn't stop us with a bad return value
 
+    with safe_cd(SRC):
         if IS_DJANGO:
-            django_bits = " --load-plugins pylint_django"
+            django_bits = "--load-plugins pylint_django "
         else:
             django_bits = ""
 
-        command = "{0} pylint{1} --rcfile=pylintrc.ini {2}".format(PIPENV, django_bits, PROJECT_NAME).strip()
+        # command += "{0}--rcfile=pylintrc.ini {1}".format(django_bits, PROJECT_NAME).split(" ")
+        command = (
+            "{0} pylint {1} --rcfile=pylintrc.ini {2}".format(
+                PIPENV, django_bits, PROJECT_NAME
+            )
+            .strip()
+            .replace("  ", " ")
+        )
         print(command)
         command = command.split(" ")
 
         # keep out of src tree, causes extraneous change detections
-        lint_output_file_name = "../lint.txt"
+        lint_output_file_name = "lint.txt"
         with open(lint_output_file_name, "w") as outfile:
-            # Ths is set up to supress lint failing on even 1 line of lint.
-            # but that doesn't distinguish betweet lint or ImportErrors!
-            my_env = config_pythonpath()
-            #subprocess.run(command, stdout=outfile, env=my_env, timeout=120)
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, env=my_env)
-            for line in iter(process.stdout.readline, b''):  # replace '' with b'' for Python 3
-                if b"memoize.py" in line:
-                    continue
-                sys.stdout.write(line.decode())
-                outfile.write(line.decode())
+            env = config_pythonpath()
+            subprocess.call(command, stdout=outfile, env=env)
 
-        fatal_errors = sum(1 for line in open(lint_output_file_name)
-                           if "no-member" in line or \
-                           "no-name-in-module" in line or \
-                           "import-error" in line)
+        fatal_errors = sum(
+            1
+            for line in open(lint_output_file_name)
+            if "no-member" in line
+            or "no-name-in-module" in line
+            or "import-error" in line
+        )
 
         if fatal_errors > 0:
             for line in open(lint_output_file_name):
-                if "no-member" in line or \
-                        "no-name-in-module" in line or \
-                        "import-error" in line:
+                if (
+                    "no-member" in line
+                    or "no-name-in-module" in line
+                    or "import-error" in line
+                ):
                     print(line)
 
             print("Fatal lint errors : {0}".format(fatal_errors))
             exit(-1)
-            return
 
-        num_lines = sum(1 for line in open(lint_output_file_name)
-                        if "*************" not in line
-                        and "---------------------" not in line
-                        and "Your code has been rated at" not in line)
-
-        got_worse = is_it_worse("lint", num_lines, margin=10)
-        max_lines = 350
-        if num_lines > max_lines or got_worse:
-            if got_worse:
-                print("lint got worse - lines : {0}".format(num_lines))
-            else:
-                print("Too many lines of lint : {0} out of max of {1}".format(num_lines, max_lines))
-            exit(-1)
+        cutoff = 100
+        num_lines = sum(
+            1
+            for line in open(lint_output_file_name)
+            if "*************" not in line
+            and "---------------------" not in line
+            and "Your code has been rated at" not in line
+        )
+        if num_lines > cutoff:
+            raise TypeError(
+                "Too many lines of lint : {0}, max {1}".format(num_lines, cutoff)
+            )
 
 
 @task(lint)
@@ -151,7 +179,9 @@ def nose_tests():
     with safe_cd(SRC):
         if IS_DJANGO:
             # Django app
-            command = "{0} {1} manage.py test {2} -v 2".format(PIPENV, PYTHON, PROJECT_NAME).strip()
+            command = "{0} {1} manage.py test {2} -v 2".format(
+                PIPENV, PYTHON, PROJECT_NAME
+            ).strip()
             # We'd expect this to be MAC or a build server.
             my_env = config_pythonpath()
             execute_with_environment(command, env=my_env)
@@ -166,16 +196,19 @@ def nose_tests():
 
 
 def config_pythonpath():
+    """
+    Add to PYTHONPATH
+    """
     if check_is_aws():
         env = "DEV"
     else:
         env = "MAC"
-    my_env = {**os.environ, 'ENV': env}
-    my_env["PYTHONPATH"] = my_env.get("PYTHONPATH",
-                                      "") + MAC_LIBS
+    my_env = {"ENV": env}
+    for key, value in os.environ.items():
+        my_env[key] = value
+    my_env["PYTHONPATH"] = my_env.get("PYTHONPATH", "") + MAC_LIBS
     print(my_env["PYTHONPATH"])
     return my_env
-
 
 
 @task()
@@ -188,9 +221,17 @@ def dead_code():
     with safe_cd(SRC):
         exclusions = "--exclude *settings.py,migrations/,*models.py,*_fake.py,*tests.py,*ui/admin.py"
         if IS_TRAVIS:
-            command = "{0} vulture {1} {2}".format(PYTHON, PROJECT_NAME, exclusions).strip().split()
+            command = (
+                "{0} vulture {1} {2}".format(PYTHON, PROJECT_NAME, exclusions)
+                .strip()
+                .split()
+            )
         else:
-            command = "{0} vulture {1} {2}".format(PIPENV, PROJECT_NAME, exclusions).strip().split()
+            command = (
+                "{0} vulture {1} {2}".format(PIPENV, PROJECT_NAME, exclusions)
+                .strip()
+                .split()
+            )
 
         output_file_name = "dead_code.txt"
         with open(output_file_name, "w") as outfile:
@@ -200,8 +241,12 @@ def dead_code():
         cutoff = 120
         num_lines = sum(1 for line in open(output_file_name) if line)
         if num_lines > cutoff:
-            print("Too many lines of dead code : {0}, max {1}".format(num_lines, cutoff))
+            print(
+                "Too many lines of dead code : {0}, max {1}".format(num_lines, cutoff)
+            )
             exit(-1)
+
+
 @task()
 @timed()
 def coverage():
@@ -218,63 +263,80 @@ def coverage():
         # DJANGO_SETTINGS_MODULE = core.settings
         # python_files = tests.py test_*.py *_tests.py test*_*.py *_test*.py
         if not os.path.exists("pytest.ini") and IS_DJANGO:
-            print("pytest.ini MUST exist for Django test detection or too few tests are found.")
+            print(
+                "pytest.ini MUST exist for Django test detection or too few tests are found."
+            )
             exit(-1)
             return
 
         my_env = config_pythonpath()
         command = "{0} py.test {1} --cov={2} --cov-report html:coverage --cov-fail-under 55  --verbose".format(
-            PIPENV,
-            "test", PROJECT_NAME)
+            PIPENV, "test", PROJECT_NAME
+        )
         execute_with_environment(command, my_env)
 
 
 @task()
 @timed()
 def pip_check():
-    print("pip_check always reruns")
-    with safe_cd(SRC):
+    """
+    Are packages ok?
+    """
+    execute("pip", "check")
+    if PIPENV and not IS_TRAVIS:
         execute("pipenv", "check")
+    execute("safety", "check", "-r", "requirements_dev.txt")
 
 
 @task()
 @skip_if_no_change("mypy")
-@timed()
 def mypy():
+    """
+    Are types ok?
+    """
+    if sys.version_info < (3, 4):
+        print("Mypy doesn't work on python < 3.4")
+        return
+    if IS_TRAVIS:
+        command = "{0} -m mypy {1} --ignore-missing-imports --strict".format(
+            PYTHON, PROJECT_NAME
+        ).strip()
+    else:
+        command = "{0} mypy {1} --ignore-missing-imports --strict".format(
+            PIPENV, PROJECT_NAME
+        ).strip()
 
-    command = "{0} mypy {1} --ignore-missing-imports --strict".format(PIPENV, PROJECT_NAME).strip()
-    bash_process = subprocess.Popen(command.split(" "),
-                                    # shell=True,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE
-                                    )
-    out = bash_process.communicate()[0]  # wait
-    errors_file = "mypy_errors.txt"
-
-    def is_third_party(value: str) -> bool:
-        for directory in []:
-            if value.startswith(directory):
-                return True
-        return False
-    with open(errors_file, "w+") as file_handle:
+    bash_process = subprocess.Popen(
+        command.split(" "),
+        # shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = bash_process.communicate()  # wait
+    mypy_file = "mypy_errors.txt"
+    with open(mypy_file, "w+") as lint_file:
         lines = out.decode().split("\n")
         for line in lines:
-            if is_third_party(line):
+            if "build_utils.py" in line:
                 continue
-            # if "/migrations/" in line:
-            #     continue
+            if "test.py" in line:
+                continue
+            if "tests.py" in line:
+                continue
+            if "/test_" in line:
+                continue
+            if "/tests_" in line:
+                continue
+            else:
+                lint_file.writelines([line + "\n"])
 
-            file_handle.write(line +"\n")
+    num_lines = sum(1 for line in open(mypy_file) if line and line.strip(" \n"))
+    max_lines = 25
+    if num_lines > max_lines:
+        raise TypeError(
+            "Too many lines of mypy : {0}, max {1}".format(num_lines, max_lines)
+        )
 
-    num_lines = sum(1 for _ in open(errors_file))
-    got_worse = is_it_worse("mypy", num_lines, margin=10)
-    max_lines = 500
-    if num_lines > max_lines or got_worse:
-        if got_worse:
-            print("mypy got worse - lines : {0}".format(num_lines))
-        else:
-            print("Too many lines of mypy : {0} out of max of {1}".format(num_lines, max_lines))
-        exit(-1)
 
 @task()
 @skip_if_no_change("detect_secrets", expect_files="detect-secrets-results.txt")
@@ -287,13 +349,16 @@ def detect_secrets():
     # to ignore a false posites
     errors_file = "detect-secrets-results.txt"
 
-    command = "detect-secrets scan --base64-limit 4 --exclude .idea|lock.json|lint.txt|{0}".format(errors_file)
+    command = "detect-secrets scan --base64-limit 4 --exclude .idea|lock.json|lint.txt|{0}".format(
+        errors_file
+    )
     print(command)
-    bash_process = subprocess.Popen(command.split(" "),
-                                    # shell=True,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE
-                                    )
+    bash_process = subprocess.Popen(
+        command.split(" "),
+        # shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     out = bash_process.communicate()[0]  # wait
 
     with open(errors_file, "w+") as file_handle:
@@ -310,7 +375,9 @@ def detect_secrets():
         if data["results"]:
             for result in data["results"]:
                 print(result)
-                print("detect-secrets has discovered high entropy strings, possibly passwords?")
+                print(
+                    "detect-secrets has discovered high entropy strings, possibly passwords?"
+                )
                 exit(-1)
     except JSONDecodeError:
         pass
@@ -320,7 +387,11 @@ def detect_secrets():
 @timed()
 def compile_md():
     with safe_cd(SRC):
-        execute("pandoc", *("--from=markdown --to=rst --output=README.rst README.md".split(" ")))
+        execute(
+            "pandoc",
+            *("--from=markdown --to=rst --output=README.rst README.md".split(" "))
+        )
+
 
 @task()
 @timed()
@@ -328,18 +399,24 @@ def jiggle_version():
     with safe_cd(SRC):
         command = "pip install jiggle_version --upgrade"
         execute(*(command.split(" ")))
-        command = "{0} jiggle_version here --module={1}".format(PIPENV, PROJECT_NAME).strip()
+        command = "{0} jiggle_version here --module={1}".format(
+            PIPENV, PROJECT_NAME
+        ).strip()
         result = execute_get_text(command)
         print(result)
-        command = "{0} jiggle_version find --module={1}".format(PIPENV, PROJECT_NAME).strip()
+        command = "{0} jiggle_version find --module={1}".format(
+            PIPENV, PROJECT_NAME
+        ).strip()
         result = execute_get_text(command)
         print(result)
-
 
 
 @task()
 @timed()
 def pin_dependencies():
+    """
+    Create requirement*.txt
+    """
     with safe_cd(SRC):
         execute(*("{0} pipenv_to_requirements".format(PIPENV).strip().split(" ")))
 
@@ -354,10 +431,28 @@ def check_setup_py():
         if IS_TRAVIS:
             execute(PYTHON, *("setup.py check -r -s".split(" ")))
         else:
-            execute(*("{0} {1} setup.py check -r -s".format(PIPENV, PYTHON).strip().split(" ")))
+            execute(
+                *(
+                    "{0} {1} setup.py check -r -s".format(PIPENV, PYTHON)
+                    .strip()
+                    .split(" ")
+                )
+            )
 
 
-@task(pin_dependencies, dead_code, check_setup_py, compile_md, compile_py, mypy, lint, coverage, nose_tests, jiggle_version, detect_secrets)
+@task(
+    # pin_dependencies,
+    dead_code,
+    check_setup_py,
+    compile_md,
+    compile_py,
+    mypy,
+    lint,
+    # coverage,
+    nose_tests,
+    jiggle_version,
+    detect_secrets,
+)
 @skip_if_no_change("package")
 @timed()
 def package():
@@ -370,6 +465,7 @@ def package():
 
     os.system('say "package complete."')
 
+
 @task()
 @skip_if_no_change("upload_package")
 @timed()
@@ -381,7 +477,9 @@ def upload_package():
         if IS_TRAVIS:
             pass
         else:
-            execute(*("{0} {1} setup.py upload".format(PIPENV, PYTHON).strip().split(" ")))
+            execute(
+                *("{0} {1} setup.py upload".format(PIPENV, PYTHON).strip().split(" "))
+            )
 
 
 @task()

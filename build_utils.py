4c21abd08c21637254e09baa80d548e095cb480e
Vladimir L <vladimir.losk90@gmail.com>
2019-03-10 19:45:06 -0400

    some fixes for Travis passing

--- build_utils.py
+++ build_utils.py
@@ -13,25 +13,23 @@ from checksumdir import dirhash
 from semantic_version import Version
 
 PROJECT_NAME = "tgeraser"
-PIPENV = "pipenv run"
-SRC = '.'
-PYTHON = "python3.7"
-IS_DJANGO = False
-IS_TRAVIS = 'TRAVIS' in os.environ # I double we will ever use travis...
+SRC = "."
+PYTHON = "python"
+IS_TRAVIS = "TRAVIS" in os.environ  # I double we will ever use travis...
 if IS_TRAVIS:
     PIPENV = ""
 else:
     PIPENV = "pipenv run"
-IS_APP = True # freeze requirements
+
+CURRENT_HASH = None
+
 
 def check_is_aws():
     """
-
     :rtype: bool
     """
     name = socket.getfqdn()
     return "ip-" in name and ".ec2.internal" in name
-
 
 
 # bash to find what has change recently
@@ -39,7 +37,7 @@ def check_is_aws():
 class BuildState(object):
     def __init__(self, what, where):
         self.what = what
-        self.where = where.strip("/")
+        self.where = where
         if not os.path.exists(".build_state"):
             os.makedirs(".build_state")
         self.state_file_name = ".build_state/last_change_{0}.txt".format(what)
@@ -66,11 +64,14 @@ class BuildState(object):
         # if CURRENT_HASH is None:
         # print("hashing " + directory)
         # print(os.listdir(directory))
-        CURRENT_HASH = dirhash(directory, 'md5', ignore_hidden=True,
-                               # changing these exclusions can cause dirhas to skip EVERYTHING
-                               excluded_files=[".coverage", "lint.txt"],
-                               excluded_extensions=[".pyc"]
-                               )
+        CURRENT_HASH = dirhash(
+            directory,
+            "md5",
+            ignore_hidden=True,
+            # changing these exclusions can cause dirhas to skip EVERYTHING
+            # excluded_files=[".coverage", "lint.txt"],
+            excluded_extensions=[".pyc"],
+        )
 
         print("Searching " + self.state_file_name)
         if os.path.isfile(self.state_file_name):
@@ -129,10 +130,14 @@ def skip_if_no_change(name, expect_files=None):
 
 def execute_with_environment(command, env):
     # Python 2 code! Python 3 uses context managers.
-    shell_process = subprocess.Popen(command.strip().replace("  ", " ").split(" "), env=env)
+    shell_process = subprocess.Popen(
+        command.strip().replace("  ", " ").split(" "), env=env
+    )
     value = shell_process.communicate()  # wait
     if shell_process.returncode != 0:
-        print("Didn't get a zero return code, got : {0}".format(shell_process.returncode))
+        print(
+            "Didn't get a zero return code, got : {0}".format(shell_process.returncode)
+        )
         exit(-1)
         # raise TypeError("Didn't get a zero return code, got : {0}".format(shell_process.returncode))
     return value
@@ -145,20 +150,32 @@ def execute_get_text(command):  # type: (str) ->str
     :return:
     """
     try:
-        completed = subprocess.run(
-            command,
-            check=True,
-            shell=True,
-            stdout=subprocess.PIPE,
-        )
-    except subprocess.CalledProcessError as err:
-        raise err
-    else:
-        return completed.stdout.decode('utf-8')
+        _ = subprocess.run
+        try:
+            completed = subprocess.run(
+                command,
+                check=True,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+        except subprocess.CalledProcessError as cpe:
+            raise
+        else:
+            return completed.stdout.decode("utf-8") + completed.stderr.decode("utf-8")
+    except AttributeError:
+        try:
+            p = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+            out, err = p.communicate()
+        except subprocess.CalledProcessError as cpe:
+            raise
+        else:
+            return str(out) + str(err)
 
 
 def timed():
     """This decorator prints the execution time for the decorated function."""
+
     def real_decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
@@ -167,21 +184,7 @@ def timed():
             end = time.time()
             print("{} ran in {}s".format(func.__name__, round(end - start, 2)))
             return result
+
         return wrapper
+
     return real_decorator
-
-def is_it_worse(task_name:str, current_rows:int, margin:int)->bool:
-    if not os.path.exists(".build_state"):
-        os.makedirs(".build_state")
-    file_name = ".build_state/last_count_{0}.txt".format(task_name)
-
-    last_rows =sys.maxsize
-    if os.path.isfile(file_name):
-        with open(file_name, "r+") as file:
-            last_rows = int(file.read())
-            if last_rows != current_rows:
-                file.seek(0)
-                file.write(str(current_rows))
-                file.truncate()
-
-    return current_rows > (last_rows + margin)
